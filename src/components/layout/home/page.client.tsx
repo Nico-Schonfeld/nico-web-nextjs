a24d3cdf126bde42f07b63e5a33d18fcadbadab1
Nico-Schonfeld <nicolas.schonfeld@25watts.com.ar>
2024-05-28 09:25:51 -0300

    add skills

--- src/components/layout/home/page.client.tsx
+++ src/components/layout/home/page.client.tsx
@@ -5,6 +5,11 @@ import texts from "@/messages/es.json";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 
 const HomeClient = () => {
   const text = texts.layout;
@@ -22,11 +27,58 @@ const HomeClient = () => {
           {text.home.subTitle}
         </h2>
         <p className="max-w-[40rem] text-balance">{text.home.description}</p>
-        <Button variant="link" className="px-0">
-          <Link href="/about" className="text-green-500 text-sm">
-            More...
-          </Link>
-        </Button>
+
+        <Collapsible>
+          <CollapsibleTrigger className="px-0 mb-3 mt-3 stext-sm flex items-center gap-2">
+            Ver más
+            <svg
+              width="13"
+              height="13"
+              stroke-linejoin="round"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M4.21968 3.46975L3.68935 4.00008L4.75001 5.06074L5.28034 4.53041L7.24999 2.56076L7.24999 13.4393L5.28031 11.4697L4.74998 10.9393L3.68932 12L4.21965 12.5303L7.29288 15.6036C7.6834 15.9941 8.31657 15.9941 8.70709 15.6036L11.7803 12.5303L12.3106 12L11.25 10.9393L10.7197 11.4697L8.74999 13.4393L8.74999 2.56072L10.7197 4.53041L11.25 5.06075L12.3107 4.00009L11.7803 3.46975L8.70712 0.396531C8.31659 0.00600645 7.68343 0.00600637 7.2929 0.396531L4.21968 3.46975Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </CollapsibleTrigger>
+          <CollapsibleContent>
+            <ul className="text-sm flex flex-col gap-2 items-start">
+              <li>
+                Conocimientos de programación{" "}
+                <span className="font-semibold">Frontend</span> (Preferentemente
+                React.js)
+              </li>
+              <li>
+                Programación <span className="font-semibold">Backend</span> (API
+                REST, Frameworks como Next.js o Express)
+              </li>
+              <li>
+                <span className="font-semibold">Base de Datos</span>{" "}
+                relacionales (MySQL)
+              </li>
+              <li>
+                Versionado de código <span className="font-semibold">GIT</span>
+                (GitFlow)
+              </li>
+              <li>
+                {" "}
+                <span className="font-semibold">Docker</span>
+              </li>
+              <li>
+                Metodologías ágiles (
+                <span className="font-semibold">SCRUM</span>)
+              </li>
+              <li>
+                Herramientas <span className="font-semibold">Atlassian</span>{" "}
+                (Jira, Confluence, BitBucket)
+              </li>
+            </ul>
+          </CollapsibleContent>
+        </Collapsible>
 
         <ul className="mt-7 mb-10">
           {text.home.socialMediaTopHome.map((item) => (
